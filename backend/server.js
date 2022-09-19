67e0d8a1b2a49f4a549fc8c755600b8c3e7e70d6
hannattaylor <hannataylor1993@gmail.com>
2022-09-19 15:52:41 +0200

    signup fixes

--- backend/server.js
+++ backend/server.js
@@ -1,68 +1,62 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const nedb = require('nedb-promise')
-const bcryptFunction = require('bcrypt.js')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const nedb = require("nedb-promise");
+const bcryptFunction = require("./bcrypt");
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(cors({
-    origin: '*'
-}))
+app.use(express.json());
 
-app.use (express.json())
-
-const accountsDB = new nedb ({filename: 'accounts.db', autoload : true })
-
+const accountsDB = new nedb({ filename: "accounts.db", autoload: true });
 
 //sign up
-app.post('/api/signup', async (request, response)=>{
-    const credentials = request.body
+app.post("/api/signup", async (request, response) => {
+  const credentials = request.body;
 
-    const resObj ={
-        success : true,
-        userNameExist : false,
-        userEmailExist : false,
-    }
+  const resObj = {
+    success: true,
+    userNameExist: false,
+    userEmailExist: false,
+  };
 
-    const findUser = await accountsDB.find ({
-        username : credentials.username
-    })
-    const findEmail = await accountsDB.find ({
-        email : credentials.email
-    })
+  const findUser = await accountsDB.find({
+    username: credentials.username,
+  });
+  const findEmail = await accountsDB.find({
+    email: credentials.email,
+  });
 
-    if(findUser > 0 || findEmail > 0) {
-        resObj.success = false,
-        resObj.userNameExist=true,
-        resObj.userEmailExist=true
+  if (findUser.length > 0) {
+    resObj.userNameExist = true;
+  }
 
-    }else{
-        const hashPass = await bcryptFunction.hashPassword(credentials.password)
-        credentials.password = hashPass
-        accountsDB.insert(credentials)
-
-    }
-    response.json(resObj)
-
-})
-
-
-
+  if (findEmail.length > 0) {
+    resObj.userEmailExist = true;
+  }
+  if (resObj.userEmailExist || resObj.userNameExist) {
+    resObj.success = false;
+  } else {
+    resObj.success = true;
+    const hashPass = await bcryptFunction.hashPassword(credentials.password);
+    credentials.password = hashPass;
+    accountsDB.insert(credentials);
+  }
+  response.json(resObj);
+});
 
 //login
 
-
 //add photo
 
-
-//delete photo 
-
+//delete photo
 
 //log out
 
-
-
-
-app.listen(2009,() =>{
-    console.log('Server listening to port 2009')
-})
+app.listen(2009, () => {
+  console.log("Server listening to port 2009");
+});
